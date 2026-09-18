632cbf18e62a864c9637baa1c566dcf12395c1f1
A949287 <saif-mohamed.k.external@atos.net>
2026-09-18 13:27:41 +0530

    CHORE - Remove unnecessary files and agent scripts leaving only adk folder and config files

--- tools/__init__.py
+++
@@ -1,4 +0,0 @@
-from tools.embedded_features import system_tools
-from tools.os_tools import os_tools
-from tools.application_tools import application_tools
-from tools.browser_tools import browser_tools
